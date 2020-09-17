9cba6157eefa84edc18ae60e7f55a657630c62a5
Iris Oh <mjo368@nyu.edu>
2020-09-18 00:12:29 +0900

    Added all current project files

---
+++ Assets/ChickMOVE.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickMOVE : MonoBehaviour
+{
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+       // public float speed = 0.1f // global value for the project
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.RightArrow)) 
+       {
+         
+          GetComponent<Transform>().Translate(new Vector3(1, 0, 0) * 0.05f ); 
+         //or I could write transform.Translate(new Vector3.right * speed); 
+            GetComponent<Animator>().Play("walk_right");
+      
+       } else if (Input.GetKey(KeyCode.LeftArrow))
+
+       {
+            GetComponent<Transform>().Translate(new Vector3(-1, 0, 0) * 0.05f ); 
+            //or I could write transform.Translate(new Vector3.left * 0.1f ); 
+            GetComponent<Animator>().Play("walk_left");
+     
+       } else if (Input.GetKey(KeyCode.DownArrow))
+        { 
+             GetComponent<Transform>().Translate(new Vector3(0, -1, 0 ) *0.05f ); 
+             GetComponent<Animator>().Play("jump");
+        
+        
+        } else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Transform>().Translate(new Vector3(0, 1, 0) * 0.05f ); 
+             GetComponent<Animator>().Play("jump");
+        } else 
+        {
+             GetComponent<Animator>().Play("idle");
+
+        }
+
+
+       
+
+    }
+}
---
+++ Assets/Scenes/Convo2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Convo2 : MonoBehaviour
+{
+     public GameObject dialogBox; 
+    public Text dialogText; 
+    public string dialog; 
+    public bool chick2InRange; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && chick2InRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false); 
+            } else { 
+                    dialogBox.SetActive(true); 
+                    dialogText.text = dialog; 
+
+            }
+
+            }
+
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("chick2"))
+        {
+            chick2InRange = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("chick2"))
+        {
+            chick2InRange = false; 
+            dialogBox.SetActive(false); 
+        }
+    }
+
+}
